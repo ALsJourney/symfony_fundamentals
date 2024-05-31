f33a8a993b579b0600adf7353dc8b88fb3b74890
ALsJourney <63744576+ALsJourney@users.noreply.github.com>
2024-05-31 18:57:08 +0200

    Next: services, we did serializing json at custon api endpoints

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $starshipCount = 457;
+
+        $myShip = [
+            'Testing' => 'WOOW',
+            'henlo' => 'XD XD XD',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
+    }
+}
